187a7c7fd8a359b3878b89b7c8919abbd7babf2d
damianruizz <damian.ruiz@outlook.es>
2016-10-18 13:23:28 -0500

    Add seed user to Production

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(AlumnoTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
     }
 }
